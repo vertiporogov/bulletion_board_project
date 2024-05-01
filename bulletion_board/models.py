f4c4d87a3923d799cbe739fa85986af708c4fd62
alexander <vertiporogov.alexandr@mail.ru>
2024-05-01 11:17:39 +0300

    create models Ad and Feedback

---
+++ bulletion_board/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.conf import settings
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название товара')
+    price = models.PositiveIntegerField(verbose_name='цена товара')
+    description = models.TextField(verbose_name='описание товара', **NULLABLE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='автор объявления')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата и время создания объявления')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'объявление'
+        verbose_name_plural = 'объявления'
+        ordering = ['-created_at']
+
+
+class Feedback(models.Model):
+    text = models.TextField(verbose_name='текст отзыва')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='автор отзыва')
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, verbose_name='обьявление')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата и время создания отзыва')
+
+    def __str__(self):
+        return f'{self.author} прокоментировал {self.ad.title}'
+
+    class Meta:
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'отзывы'
--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'users',
+    'bulletion_board',
 
     'rest_framework',
     'drf_yasg',
