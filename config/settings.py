44da3296b6040ce0e0870cce27d30a3db04fc837
alexander <vertiporogov.alexandr@mail.ru>
2024-05-02 20:32:09 +0300

    finished

--- config/settings.py
+++ config/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_USER'),
-        'USER': os.getenv('POSTGRES_DB'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': os.getenv('POSTGRES_PORT'),
